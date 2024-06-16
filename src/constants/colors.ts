c9f93ca87eb23adfc513ba58a94c2592afe0cd3c
sivog <sivograkovkirill@mail.ru>
2024-06-16 23:59:00 +0300

    create colors constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+   primary: 'rgb(57, 57, 57)',
+   secondary: 'rgb(241, 121, 0)',
+   boxShadow: 'rgba(0, 0, 0, 0.2)',
+   backgroundActive: '#ffcc80',
+   backgroundHoverFav: '#ffb74d',
+   backgroundHoverNotFav: '#e0e0e0',
+   background: '#F9F9F9',
+   gold: '#FBD7B24D',
+   white: '#ffffff',
+   gradient: 'linear-gradient(90deg, #343333 38.05%, #484848 69.22%, #282828 98.98%)',
+   yellow: '#e0a449',
+   black: '#000',
+   grey: '#f5f5f5',
+};
